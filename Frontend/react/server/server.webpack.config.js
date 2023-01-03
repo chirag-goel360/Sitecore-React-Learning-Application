b6082160203b8c8487a545c48ad7589c85ca3663
chirag-goel360 <lastknight03111998@gmail.com>
2023-01-03 19:11:44 +0530

    initial setup frontend

---
+++ Frontend/react/server/server.webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const env = require('@babel/preset-env');
+const reactApp = require('babel-preset-react-app');
+const webpack = require('webpack');
+// Webpack build configuration to build the SSR bundle.
+// Invoked by build:server.
+
+module.exports = {
+  name: 'server-config',
+  mode: 'production',
+  entry: path.resolve(__dirname, './server.js'),
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'server.bundle.js',
+    libraryTarget: 'this',
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@babel/plugin-proposal-export-namespace-from'],
+          },
+        },
+      },
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [env, reactApp],
+            plugins: [
+              ['@babel/plugin-proposal-private-methods', { loose: true }],
+              ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: { loader: 'html-loader' },
+      },
+      {
+        // anything not JS or HTML, we load as a URL
+        // this makes static image imports work with SSR
+        test: /\.(?!js|mjs|jsx|html|graphql$)[^.]+$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+      {
+        // anything in node_modules that isn't js,
+        // we load as null - e.g. imported css from a module,
+        // that is not needed for SSR
+        test: /\.(?!js|mjs|jsx|html|graphql$)[^.]+$/,
+        include: /node_modules/,
+        use: {
+          loader: 'null-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    // prevents the following warning during build:
+    // > WARNING in ./node_modules/encoding/lib/iconv-loader.js
+    // > Critical dependency: the request of a dependency is an expression
+    new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, () => {}),
+  ],
+};
